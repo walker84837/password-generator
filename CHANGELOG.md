c4f11e3eb4155d4d44afa6e16a03c84f6350f542
#84837 <walker84837@gmail.com>
2023-11-03 21:30:14 +0100

    Rework repo (#2)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,51 @@
 # Changelog
 
-All notable changes to the Password Generator project will be documented in this file.
+All notable changes to this project will be documented in this file.
 
-## \[1.0.0\] - 17th June 2023
+The format is based on [Keep a Changelog](https://keepachangelog.com/),
+and this project adheres to [Semantic Versioning](https://semver.org/).
 
-### What's new
+## [Unreleased]
+
+## \[1.0.1\] - 2023-11-03
+
+### Added
+
+- Added the [Code of conduct](CODE_OF_CONDUCT.md) file.
+
+### Fixed
+
+- The password generator will now generate the correct password format.
+
+### Changed
+
+- The license is now dual-licensed in MIT License or Apache License, Version 2.
+- Formatting style is now based on K&R.
+- The CHANGELOG.md file now uses Keep a Changelog's changelog format.
+- Rewrote the README.md file.
+
+### Removed
+
+- None.
+
+## \[1.0.0\] - 2023-06-17
+
+### Added
 
 - Initial release of the password-generator.
 - Basic password generation functionality.
 - Copy to clipboard feature.
 - Notification system for password generation success/failure.
+
+### Fixed
+
+- None.
+
+### Changed
+
+- None.
+
+### Removed
+
+- None.
+
--- password.js
+++ password.js
@@ -1,48 +1,43 @@
-function getRandomIndex(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+function getRandomIndex(min, max)
+{
+	return Math.floor(Math.random() * (max - min) + min);
 }
 
-function generatePassword() {
-  let lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-  let uppercaseLetters = lowercaseLetters.toUpperCase();
-  let numbers = "0123456789";
+function generatePassword()
+{
+	let lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+	let uppercaseLetters = lowercaseLetters.toUpperCase();
+	let numbers = "0123456789";
 
-  let password = "";
+	let password = "";
 
-  // Generate 6 lowercase letters
-  for (let i = 0; i < 6; i++) {
-    password += lowercaseLetters.charAt(getRandomIndex(0, lowercaseLetters.length));
-  }
+	// Generate 6 lowercase letters
+	for (let i = 0; i < 6; i++) {
+		password += lowercaseLetters.charAt(getRandomIndex(0, lowercaseLetters.length));
+	}
 
-  // Generate a number
-  password += numbers.charAt(getRandomIndex(0, numbers.length));
+	// Generate a number
+	password += numbers.charAt(getRandomIndex(0, numbers.length));
 
-  // Generate 9 more lowercase letters
-  for (let i = 0; i < 9; i++) {
-    password += lowercaseLetters.charAt(getRandomIndex(0, lowercaseLetters.length));
-  }
+	// Generate 9 more lowercase letters
+	for (let i = 0; i < 9; i++) {
+		password += lowercaseLetters.charAt(getRandomIndex(0, lowercaseLetters.length));
+	}
 
-  // Generate 6 more lowercase letters
-  for (let i = 0; i < 6; i++) {
-    password += lowercaseLetters.charAt(getRandomIndex(0, lowercaseLetters.length));
-  }
+	// Generate 6 more lowercase letters
+	for (let i = 0; i < 6; i++) {
+		password += lowercaseLetters.charAt(getRandomIndex(0, lowercaseLetters.length));
+	}
 
-  // Generate random position for uppercase letter
-  let uppercaseIndex = getRandomIndex(0, password.length + 1);
+	// Generate random position for uppercase letter
+	let uppercaseIndex = getRandomIndex(0, password.length + 1);
 
-  // Insert uppercase letter at the random position
-  password =
-    password.slice(0, uppercaseIndex) +
-    uppercaseLetters.charAt(getRandomIndex(0, uppercaseLetters.length)) +
-    password.slice(uppercaseIndex);
+	// Insert uppercase letter at the random position
+	password = password.slice(0, uppercaseIndex) + uppercaseLetters.charAt(getRandomIndex(0, uppercaseLetters.length)) +
+		password.slice(uppercaseIndex);
 
-  // Insert dashes at specific positions
-  password =
-    password.slice(0, 6) +
-    "-" +
-    password.slice(6, 13) +
-    "-" +
-    password.slice(13, 20);
+	// Insert dashes at specific positions
+	password = password.slice(0, 6) + "-" + password.slice(6, 12) + "-" + password.slice(12, 19);
 
-  document.getElementById("password").value = password;
+	document.getElementById("password").value = password;
 }
